f430c738d6a6551cc229951469c21344e2f074c4
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-06-15 10:50:34 +0200

    Travis, browserstack

---
+++ test-e2e/sample.test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const assert = require('chai').assert;
+
+describe('Google\'s Search Functionality', function() {
+  it('can find search results', function () {
+    browser
+      .url('https://www.google.com/ncr')
+      .setValue('*[name="q"]','BrowserStack\n')
+      .pause(5000);
+
+    assert(browser.getTitle().match(/BrowserStack - Google Search/i));
+  });
+});
---
+++ wdio.conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+
+  updateJob: false,
+  specs: [
+    './test-e2e/**/*.test.js'
+  ],
+  capabilities: [{
+    browser: 'chrome'
+  }],
+
+  logLevel: 'verbose',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd'
+  }
+};
