0068f8cb4a0904e67a46df72a746c123eee6fa2f
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-06-21 09:28:14 +0200

    Additional testing

--- test-e2e/sample.test.js
+++ test-e2e/sample.test.js
@@ -12,8 +12,8 @@ describe('Autocomplete', function () {
     assert((await browser.getTitle()).match(/DAWA autocomplete - demo/i));
   });
 
-  it('Can complete standard autocomplete scenario', async () => {
-    browser.url('http:localhost:8080/demo-polyfilled.html');
+  it('Vælges en adgangsadresse med kun én adresse vælges adressen automatisk', async () => {
+    await browser.url('http:localhost:8080/demo-polyfilled.html');
     browser.execute(function () {
       window.focus();
     });
@@ -33,5 +33,22 @@ describe('Autocomplete', function () {
     // select using ENTER key
     await browser.keys('\uE006');
     await browser.waitUntil(async () => (await browser.getValue(inputSelector)) === 'Rentemestervej 4, 2400 København NV');
+  });
+
+  it('Hvis der vælges en adgangsadresse med flere adresser, udfyldes tekst og caretpos korrekt', async () => {
+    await browser.url('http:localhost:8080/demo-polyfilled.html');
+    browser.execute(function () {
+      window.focus();
+    });
+    const inputSelector = '#autocomplete-default';
+    // click to set focus on input
+    await browser.click(inputSelector);
+    await browser.keys('margrethepladsen ');
+    const adgadrSelector = 'div.autocomplete-suggestion=Margrethepladsen 4, 8000 Aarhus C';
+    await browser.waitForExist(adgadrSelector);
+    await browser.click(adgadrSelector);
+    await browser.waitUntil(async () => (await browser.getValue(inputSelector)) === 'Margrethepladsen 4, , 8000 Aarhus C');
+    const caretPos = await browser.selectorExecute(inputSelector, (inputElm => inputElm[0].selectionStart));
+    assert.strictEqual(caretPos, 'Margrethepladsen 4, '.length);
   });
 });
