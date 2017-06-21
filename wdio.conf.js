56d6830664ba43c1089c767bdf794f014b4d027f
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-06-21 09:56:36 +0200

    Add testing in Edge, Safari

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,10 +10,55 @@ exports.config = {
   ],
   capabilities: [{
     browser: 'chrome',
-    name: 'local_test',
+    name: 'chrome_local',
     build: 'webdriver-browserstack',
     'browserstack.local': true
-  }],
+  }
+  // , {
+  //   'os': 'Windows',
+  //   'os_version': '7',
+  //   'browser': 'IE',
+  //   'browser_version': '11.0',
+  //   'resolution': '1024x768',
+  //   name: 'IE11-local',
+  //   build: 'webdriver-browserstack',
+  //   'browserstack.local': true
+  //
+  // }
+  , {
+    'os': 'Windows',
+    'os_version': '10',
+    'browser': 'Edge',
+    'resolution': '1024x768',
+    name: 'Edge-local',
+    build: 'webdriver-browserstack',
+    'browserstack.local': true
+  }
+  , {
+    'os': 'OS X',
+    'os_version': 'Sierra',
+    'browser': 'Safari',
+    name: 'Safari-local',
+    build: 'webdriver-browserstack',
+    'browserstack.local': true
+  }
+  // , {
+  //   'browserName': 'iPhone',
+  //   'platform': 'MAC',
+  //   'device': 'iPhone 5',
+  //   name: 'Iphone5-local',
+  //   build: 'webdriver-browserstack',
+  //   'browserstack.local': true
+  // }
+  // , {
+  //   'browserName': 'android',
+  //   'platform': 'ANDROID',
+  //   'device': 'Samsung Galaxy S5',
+  //   name: 'S5-local',
+  //   build: 'webdriver-browserstack',
+  //   'browserstack.local': true
+  // }
+  ],
 
   logLevel: 'verbose',
   coloredLogs: true,
@@ -31,9 +76,9 @@ exports.config = {
 
   onPrepare: function (config, capabilities) {
     console.log("Connecting local");
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
       exports.bs_local = new browserstack.Local();
-      exports.bs_local.start({'key': exports.config.key }, function(error) {
+      exports.bs_local.start({'key': exports.config.key}, function (error) {
         if (error) return reject(error);
         console.log('Connected. Now testing...');
 
@@ -43,6 +88,7 @@ exports.config = {
   },
   // Code to stop browserstack local after end of test
   onComplete: function (capabilties, specs) {
-    exports.bs_local.stop(function() {});
+    exports.bs_local.stop(function () {
+    });
   }
 };
