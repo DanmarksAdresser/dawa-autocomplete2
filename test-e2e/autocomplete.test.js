417d055d0bb30f19f10f1147f55d0e49d072d035
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-06-26 11:32:57 +0200

    Automatic test of keyboard navigation

--- test-e2e/autocomplete.test.js
+++ test-e2e/autocomplete.test.js
@@ -44,7 +44,7 @@ describe('Autocomplete', function () {
     // click to set focus on input
     await browser.click(inputSelector);
     await browser.keys('margrethepladsen ');
-    const adgadrSelector = '.autocomplete-suggestion=Margrethepladsen 4, 8000 Aarhus C';
+    const adgadrSelector = '.dawa-autocomplete-suggestion=Margrethepladsen 4, 8000 Aarhus C';
     await browser.waitForExist(adgadrSelector);
     await browser.click(adgadrSelector);
     await browser.waitUntil(async () => (await browser.getValue(inputSelector)) === 'Margrethepladsen 4, , 8000 Aarhus C');
@@ -52,5 +52,29 @@ describe('Autocomplete', function () {
       return inputElm[0].selectionStart;
     });
     assert.strictEqual(caretPos, 'Margrethepladsen 4, '.length);
+  });
+
+  it('Kan navigere autocomplete med keyboard', async () => {
+    await browser.url('http:localhost:8080/demo-polyfilled.html');
+    browser.execute(function () {
+      window.focus();
+    });
+    const inputSelector = '#autocomplete-default';
+    await browser.click(inputSelector);
+    await browser.keys('margrethep');
+    await browser.waitForExist('.dawa-selected=Margretheparken');
+    browser.keys("ArrowDown");
+    await browser.waitForExist('.dawa-selected=Margrethepladsen');
+    // key down again should wrap to top
+    browser.keys("ArrowDown");
+    await browser.waitForExist('.dawa-selected=Margretheparken');
+    // key up should wrap to bottom
+    browser.keys("ArrowUp");
+    await browser.waitForExist('.dawa-selected=Margrethepladsen');
+    // key up
+    browser.keys("ArrowUp");
+    await browser.waitForExist('.dawa-selected=Margretheparken');
+
+
   });
 });
