935c32577ac5e2cff9419b8e9c00f54510209488
Anders Hessellund Jensen <extajn@trifork.com>
2020-01-23 17:20:09 +0100

    Bump dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 // rollup.config.js
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 
 const configs = [];
 for(let polyfilled of [true, false]) {
